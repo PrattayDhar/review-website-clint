ab8a7c347e922a34589631b94c94bc368acd04e0
PrattayDhar <prattaygub@gmail.com>
2022-11-09 11:34:59 +0000

    review

--- src/component/ServiceDetails/ServiceDetails.js
+++ src/component/ServiceDetails/ServiceDetails.js
@@ -1,28 +1,30 @@
 import React, { useContext, useEffect, useState } from 'react';
+// import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
+import './Servicedetails.css'
 
 const ServiceDetails = () => {
     const [user, setReview] = useState([])
-    const singledetails=useLoaderData()
-    const {User}=useContext(AuthContext)
-    const{_id}=singledetails
+    const singledetails = useLoaderData()
+    const { User } = useContext(AuthContext)
+    const { _id } = singledetails
     console.log(singledetails);
 
     const reviewsubmit = event => {
         event.preventDefault();
-        const form=event.target;
-        const review=form.review.value;
-        const reviewdit={
-            ServiceId:_id,
-            UserName:User?.displayName,
-            UserEmail:User?.email,
-            UserImage:User?.photoURL,
-            UserReview:review,
-            Time:new Date()
+        const form = event.target;
+        const review = form.review.value;
+        const reviewdit = {
+            ServiceId: _id,
+            UserName: User?.displayName,
+            UserEmail: User?.email,
+            UserImage: User?.photoURL,
+            UserReview: review,
+            Time: new Date()
 
         }
-       
+
         fetch("http://localhost:5000/AddReview", {
             method: 'POST',
             headers: {
@@ -44,48 +46,69 @@ const ServiceDetails = () => {
             .then((res) => res.json())
             .then((data) => setReview(data));
     }, []);
-    
+
     return (
-        <div>
-            <div className="row row-cols-2 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <img src={singledetails.img} className="card-img-top" alt="..."/>
-                            <div className="card-body">
+        <div className='d-flex'>           
+                    <div className="card w-50 h-75 mt-2 mx-2">
+                        <img src={singledetails.img} className="card-img-top" alt="..." />
+                        <div className="card-body">
                             <h5 className="card-title">{singledetails.title}</h5>
                             <p className="card-text">{singledetails.description}</p>
-                            <p className="card-text">{singledetails.price}</p></div>
+                            <p className="card-text">Price:{singledetails.price}</p></div>
                         <form onSubmit={reviewsubmit}>
-                            <div class="mb-3">
-                                <label htmlFor="review" class="form-label">Give Your Review</label>
-                                <input  type="text" class="form-control" name="review" aria-describedby="review"/>
-                                    <div id="review" class="form-text">Please Give Your Honest Review</div>
+                            <div className="mb-3">
+                                <label htmlFor="review" className="form-label">Give Your Review</label>
+                                <input type="text" className="form-control" name="review" aria-describedby="review" />
+                                <div id="review" className="form-text">Please Give Your Honest Review</div>
                             </div>
-                            
+
                             <button className="btn btn-info" type="submit" value="Submit">Submit</button>
-                           
-                            </form>
-                        
-                        </div>
-                          
+
+                        </form>
+
                     </div>
-                <div className="col">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">{user.length}</h5>
-                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+                    <div>
+
+                <div className="containers mt-5">
+                    <h3 className='mx-5 px-5'>User Review</h3>
+
+                    <div className="d-flex justify-content-center py-3">
+
+                        <div className="mr-2">
+                            {
+                                user.map(review =>
+                                    <div className="cards d-flex">
+                                        <div className="px-2 py-2">
+                                            <span className="maintxt">{review.UserReview}</span>
+                                            <div className="d-flex pt-3">
+                                                <div>
+                                                    <img className='rounded-circle w-50' src={review.UserImage} alt="User Pic" />
+                                                </div>
+                                                <div className="ml-2">
+                                                    <span className="name">{review.UserName}</span>
+                                                    <p className="para">{review.UserEmail}</p>
+                                                </div>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                )
+                            }
+
                         </div>
+
                     </div>
 
                 </div>
-                </div>
+                    </div>
                 
-              
-              
-            </div>
-       
+
+        </div>
+
     );
 };
 
 export default ServiceDetails;
+// width = "50" className="rounded-circle"
